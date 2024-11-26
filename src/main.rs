9cfb8b2ac96be57376350d34dbe5f313e46f4eee
Steven <stevenmolina2205@gmail.com>
2024-11-26 19:16:43 -0300

    refactor: uses enum for commands and user interaction

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,8 @@ fn main() {
     io::stdin().read_line(&mut input).expect("Failed to read");
     let input = input.trim();
 
-    match input {
-        "1" => stack::use_stack(),
-        "2" => iters::use_my_iter(),
-        "3" => queue::use_queue(),
-        "4" => lists::use_list(),
-        "5" => lists::use_sorte_l(),
-        "6" => graphs::use_graph(),
-        _ => (),
-    }
+    let command = Command::from_input(input.trim());
+    command.execute();
 }
 
 fn show_options() {
@@ -38,4 +31,40 @@ fn show_options() {
     println!("\tGraph: 6");
 
     println!()
+}
+
+enum Command {
+    Stack,
+    Iter,
+    Queue,
+    List,
+    SortedList,
+    Graph,
+    Invalid,
+}
+
+impl Command {
+    fn from_input(input: &str) -> Self {
+        match input {
+            "1" => Command::Stack,
+            "2" => Command::Iter,
+            "3" => Command::Queue,
+            "4" => Command::List,
+            "5" => Command::SortedList,
+            "6" => Command::Graph,
+            _ => Command::Invalid,
+        }
+    }
+
+    fn execute(&self) {
+        match self {
+            Command::Stack => stack::use_stack(),
+            Command::Iter => iters::use_my_iter(),
+            Command::Queue => queue::use_queue(),
+            Command::List => lists::use_list(),
+            Command::SortedList => lists::use_sorte_l(),
+            Command::Graph => graphs::use_graph(),
+            Command::Invalid => println!("Invalid option. Please try again."),
+        }
+    }
 }
